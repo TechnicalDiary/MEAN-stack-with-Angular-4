6ac61632cb8b5fff2c020b9e99ce224a4b795490
Shahbaz Sharif <Shahbaz.Sharif@marlabs.com>
2017-08-11 20:48:48 +0530

    added client project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MEAN-stack-with-Angular-4
 MEAN stack with Angular 4
+
+# start mongo server...
+set command path to mongo 
+run this code ==> mongod.exe --dbpath "D:\data\db"
--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const config = require('./config/database');
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/');
+mongoose.connect(config.uri, (err) => {
+    if (err){
+        console.error('could not connect to database');
+    } else {
+        console.log('connected to datbase: ' + config.db);
+        // console.log(config.secret);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist'))
 
 app.get('*', (req,res) =>{
-    res.send('Hello world');
+    res.sendFile(path.join(__dirname + client/dist/index.html));
+    // res.send('Hello world');
 });
 
 //comment
